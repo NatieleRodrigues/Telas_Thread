992d7c40b8d8348a3308e7188610c508bcdcd79f
Natiele Rodrigues <Natiele Rodrigues@192.168.100.5>
2023-05-17 09:09:28 -0300

    Git

--- primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,7 +5,7 @@ public class PrimeiraClasseJava {
 	/*Main é um metodo auto executavel em java*/
 	public static void main(String[] args) {
 		/*Escreve texto no console*/
-		System.out.println("Meu primeiro Programa HELO WORD!!!!");
+		System.out.println("Aprendendo a fazer commit no github!!!!");
 	}
 	
 }
